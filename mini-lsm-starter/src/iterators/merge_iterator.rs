b148a05c6251172ba46ad231a48e5dcfc61da319
Foreverhighness <Foreverhighness@github.com>
2024-06-19 17:17:50 +0800

    🚨 (merge_iterator): fix `clippy::non_canonical_partial_ord_impl` lint.

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -16,27 +16,25 @@ struct HeapWrapper<I: StorageIterator>(pub usize, pub Box<I>);
 
 impl<I: StorageIterator> PartialEq for HeapWrapper<I> {
     fn eq(&self, other: &Self) -> bool {
-        self.partial_cmp(other).unwrap() == cmp::Ordering::Equal
+        self.1.key().eq(&other.1.key()) && self.0.eq(&other.0)
     }
 }
 
 impl<I: StorageIterator> Eq for HeapWrapper<I> {}
 
 impl<I: StorageIterator> PartialOrd for HeapWrapper<I> {
-    #[allow(clippy::non_canonical_partial_ord_impl)]
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        match self.1.key().cmp(&other.1.key()) {
-            cmp::Ordering::Greater => Some(cmp::Ordering::Greater),
-            cmp::Ordering::Less => Some(cmp::Ordering::Less),
-            cmp::Ordering::Equal => self.0.partial_cmp(&other.0),
-        }
-        .map(cmp::Ordering::reverse)
+        Some(self.cmp(other))
     }
 }
 
 impl<I: StorageIterator> Ord for HeapWrapper<I> {
     fn cmp(&self, other: &Self) -> cmp::Ordering {
-        self.partial_cmp(other).unwrap()
+        self.1
+            .key()
+            .cmp(&other.1.key())
+            .then_with(|| self.0.cmp(&other.0))
+            .reverse()
     }
 }
 
