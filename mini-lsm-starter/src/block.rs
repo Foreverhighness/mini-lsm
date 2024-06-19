70a3fe1fd58fa74a246b41199b5e9ea9f97c065b
Foreverhighness <Foreverhighness@github.com>
2024-06-19 23:05:50 +0800

    🚨 (w1d3): remove clippy allow.

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,7 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-#![allow(clippy::must_use_candidate)] // TODO(fh): remove clippy allow
-
 mod builder;
 mod iterator;
 
