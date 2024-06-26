d049f99d4a48517f20e0e591b2704537bb57f0b6
Foreverhighness <Foreverhighness@github.com>
2024-06-26 11:02:53 +0800

    🚧 (tiered): Add apply_compaction_result implement.

--- mini-lsm-starter/src/compact/tiered.rs
+++ mini-lsm-starter/src/compact/tiered.rs
@@ -38,10 +38,63 @@ impl TieredCompactionController {
 
     pub fn apply_compaction_result(
         &self,
-        _snapshot: &LsmStorageState,
-        _task: &TieredCompactionTask,
-        _output: &[usize],
+        snapshot: &LsmStorageState,
+        task: &TieredCompactionTask,
+        output: &[usize],
     ) -> (LsmStorageState, Vec<usize>) {
-        unimplemented!()
+        let mut new_state = snapshot.clone();
+        let TieredCompactionTask {
+            ref tiers,
+            bottom_tier_included,
+        } = *task;
+
+        let deleted_sst_ids = tiers
+            .iter()
+            .flat_map(|(_, tier)| tier.iter())
+            .copied()
+            .collect::<Vec<_>>();
+        debug_assert_eq!(output.len(), deleted_sst_ids.len());
+
+        let new_tier_id = output[0];
+
+        if bottom_tier_included {
+            let new_tier_len = new_state.levels.len() - tiers.len();
+            assert_eq!(&snapshot.levels[new_tier_len..], tiers);
+
+            // Correct way is truncate then push
+            // new_state.levels.truncate(new_tier_len);
+            // new_state.levels.push((new_tier_id, output.to_owned()));
+            new_state.levels.truncate(new_tier_len + 1);
+            let new_tier = &mut new_state.levels[new_tier_len];
+            new_tier.0 = new_tier_id;
+            output.clone_into(&mut new_tier.1);
+        } else {
+            let first_tier_id = tiers[0].0;
+            let start = new_state
+                .levels
+                .iter()
+                .position(|&(tier_id, _)| tier_id == first_tier_id)
+                .unwrap();
+
+            // let end = start + tiers.len();
+            // let old_tiers = new_state.levels.drain(start..end);
+            // let old_tiers = old_tiers.collect::<Vec<_>>();
+            // debug_assert_eq!(&old_tiers, tiers);
+            // new_state
+            //     .levels
+            //     .insert(start, (new_tier_id, output.to_owned()));
+            let end = start + tiers.len() - 1;
+            let end_element = new_state.levels[end].clone();
+            let old_tiers = new_state.levels.drain(start..end);
+            let old_tiers = old_tiers
+                .chain(std::iter::once(end_element))
+                .collect::<Vec<_>>();
+            debug_assert_eq!(&old_tiers, tiers);
+            let new_tier = &mut new_state.levels[start];
+            new_tier.0 = new_tier_id;
+            output.clone_into(&mut new_tier.1);
+        }
+
+        (new_state, deleted_sst_ids)
     }
 }
