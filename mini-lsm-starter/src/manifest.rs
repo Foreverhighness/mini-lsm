42711c6dcbed6429c39a25656f1ab2dccf3bd7c1
Foreverhighness <Foreverhighness@github.com>
2024-06-27 19:42:38 +0800

    🚧 (manifest): Add manifest implement.

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)] // REMOVE THIS LINE after fully implementing this functionality
 #![allow(clippy::needless_pass_by_value)] // TODO(fh): remove clippy allow
 
-use std::fs::File;
+use std::io::BufReader;
 use std::path::Path;
 use std::sync::Arc;
+use std::{fs::File, io::Write};
 
 use anyhow::Result;
 use parking_lot::{Mutex, MutexGuard};
@@ -24,12 +25,26 @@ pub enum ManifestRecord {
 }
 
 impl Manifest {
-    pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        let file = std::fs::OpenOptions::new()
+            .append(true)
+            .create_new(true)
+            .open(path)?;
+        let file = Arc::new(Mutex::new(file));
+
+        Ok(Self { file })
     }
 
-    pub fn recover(_path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
-        unimplemented!()
+    pub fn recover(path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
+        let path = path.as_ref();
+        let file = std::fs::OpenOptions::new().read(true).open(path)?;
+        let buffer_file = BufReader::new(file);
+        let records = serde_json::from_reader(buffer_file)?;
+
+        let file = std::fs::OpenOptions::new().append(true).open(path)?;
+        let file = Arc::new(Mutex::new(file));
+
+        Ok((Self { file }, records))
     }
 
     pub fn add_record(
@@ -40,7 +55,13 @@ impl Manifest {
         self.add_record_when_init(record)
     }
 
-    pub fn add_record_when_init(&self, _record: ManifestRecord) -> Result<()> {
-        unimplemented!()
+    pub fn add_record_when_init(&self, record: ManifestRecord) -> Result<()> {
+        let mut file = self.file.lock();
+        let json = serde_json::to_vec(&record)?;
+
+        file.write_all(&json)?;
+        file.sync_all()?;
+
+        Ok(())
     }
 }
