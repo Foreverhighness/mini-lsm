8ac54f1d43f964aa3eb92217cebf45650d4ea482
Foreverhighness <Foreverhighness@github.com>
2024-06-27 18:26:26 +0800

    ✨ (leveled) Leveled Compaction.

--- mini-lsm-starter/src/compact/leveled.rs
+++ mini-lsm-starter/src/compact/leveled.rs
@@ -2,9 +2,11 @@
 #![allow(clippy::missing_const_for_fn)] // TODO(fh): remove clippy allow
 #![allow(clippy::unused_self)] // TODO(fh): remove clippy allow
 
+use std::ops::Bound;
+
 use serde::{Deserialize, Serialize};
 
-use crate::lsm_storage::LsmStorageState;
+use crate::{lsm_storage::LsmStorageState, table::SsTable};
 
 const MB: usize = 1024 * 1024;
 
@@ -36,13 +38,75 @@ impl LeveledCompactionController {
         Self { options }
     }
 
+    fn lower_upper_key<'r, 's: 'r>(
+        &self,
+        snapshot: &'s LsmStorageState,
+        sst_ids: &[usize],
+    ) -> (&'r [u8], &'r [u8]) {
+        sst_ids
+            .iter()
+            .map(|id| {
+                let sst = &snapshot.sstables[id];
+                (sst.first_key().raw_ref(), sst.last_key().raw_ref())
+            })
+            .reduce(|(lower, upper), (lo, up)| (lower.min(lo), upper.max(up)))
+            .unwrap()
+    }
+
     fn find_overlapping_ssts(
         &self,
-        _snapshot: &LsmStorageState,
-        _sst_ids: &[usize],
-        _in_level: usize,
+        snapshot: &LsmStorageState,
+        sst_ids: &[usize],
+        in_level: usize,
     ) -> Vec<usize> {
-        unimplemented!()
+        debug_assert!(!sst_ids.is_empty());
+        let (lower_key, upper_key) = self.lower_upper_key(snapshot, sst_ids);
+        let (lower, upper) = (Bound::Included(lower_key), Bound::Included(upper_key));
+
+        let level = &snapshot.levels[in_level - 1].1;
+
+        let range_overlap_with_sst = move |sst: &SsTable| {
+            let first_key = sst.first_key().raw_ref();
+            let last_key = sst.last_key().raw_ref();
+            match lower {
+                Bound::Included(left) if last_key < left => return false,
+                Bound::Excluded(left) if last_key <= left => return false,
+                _ => (),
+            }
+            match upper {
+                Bound::Included(right) if right < first_key => return false,
+                Bound::Excluded(right) if right <= first_key => return false,
+                _ => (),
+            }
+            true
+        };
+
+        let res = level
+            .iter()
+            .filter(|&id| range_overlap_with_sst(&snapshot.sstables[id]))
+            .copied()
+            .collect::<Vec<_>>();
+
+        // TODO(fh): use partition_point to optimize O(n) -> O(nlogn)
+        let _res = {
+            let mut left = level
+                .partition_point(|id| snapshot.sstables[id].first_key().raw_ref() <= lower_key)
+                .saturating_sub(1);
+            if let Some(id) = level.get(left) {
+                if snapshot.sstables[id].last_key().raw_ref() < lower_key {
+                    left += 1;
+                }
+            }
+            if !res.is_empty() {
+                debug_assert_eq!(level[left], res[0]);
+            }
+            // TODO(fh): get right right
+            let right =
+                level.partition_point(|id| snapshot.sstables[id].last_key().raw_ref() <= upper_key);
+            level[left..right].to_vec()
+        };
+
+        res
     }
 
     pub fn generate_compaction_task(
@@ -50,7 +114,7 @@ impl LeveledCompactionController {
         snapshot: &LsmStorageState,
     ) -> Option<LeveledCompactionTask> {
         let len = snapshot.levels.len();
-        let level_size = snapshot
+        let mut level_size = snapshot
             .levels
             .iter()
             .map(|(_, sst_ids)| {
@@ -87,13 +151,53 @@ impl LeveledCompactionController {
 
         if snapshot.l0_sstables.len() >= self.options.level0_file_num_compaction_trigger {
             let upper_level_sst_ids = &snapshot.l0_sstables;
-            let lower_level_sst_ids = &snapshot.levels[target_level_idx].1;
+            let lower_level_sst_ids =
+                self.find_overlapping_ssts(snapshot, upper_level_sst_ids, target_level);
             return Some(LeveledCompactionTask {
                 upper_level: None,
                 upper_level_sst_ids: upper_level_sst_ids.clone(),
                 lower_level: target_level,
-                lower_level_sst_ids: lower_level_sst_ids.clone(),
+                lower_level_sst_ids,
                 is_lower_level_bottom_level: target_level == len,
+            });
+        }
+
+        let mut multiplier = 1;
+        for idx in (target_level_idx..len).rev() {
+            level_size[idx] *= multiplier;
+            multiplier *= self.options.level_size_multiplier;
+        }
+
+        debug_assert!(level_size[len - 1] <= target_size[len - 1]);
+        if let Some((upper_level_idx, _)) = level_size
+            .iter()
+            .enumerate()
+            .filter(|&(_, &size)| size > target_size[len - 1])
+            .max_by_key(|(_, &v)| v)
+        {
+            let upper_level = upper_level_idx + 1;
+            let lower_level_idx = upper_level_idx + 1;
+            let lower_level = lower_level_idx + 1;
+
+            debug_assert_ne!(upper_level, len);
+
+            eprintln!("{upper_level} compact to {lower_level}");
+
+            let upper_level_sst_ids = snapshot.levels[upper_level_idx]
+                .1
+                .iter()
+                .min()
+                .copied()
+                .unwrap();
+            let upper_level_sst_ids = vec![upper_level_sst_ids];
+            let lower_level_sst_ids =
+                self.find_overlapping_ssts(snapshot, &upper_level_sst_ids, lower_level);
+            return Some(LeveledCompactionTask {
+                upper_level: Some(upper_level),
+                upper_level_sst_ids,
+                lower_level,
+                lower_level_sst_ids,
+                is_lower_level_bottom_level: lower_level == len,
             });
         }
 
@@ -123,8 +227,17 @@ impl LeveledCompactionController {
 
         if let Some(upper_level) = upper_level {
             debug_assert_eq!(new_state.levels[upper_level - 1].0, upper_level);
-            debug_assert_eq!(&new_state.levels[upper_level - 1].1, upper_level_sst_ids);
-            new_state.levels[upper_level - 1].1.clear();
+            debug_assert_eq!(upper_level_sst_ids.len(), 1);
+
+            let upper_level = &mut new_state.levels[upper_level - 1].1;
+            upper_level.remove(
+                upper_level
+                    .iter()
+                    .enumerate()
+                    .find(|&(_, &v)| v == upper_level_sst_ids[0])
+                    .unwrap()
+                    .0,
+            );
         } else {
             let new_l0_len = new_state.l0_sstables.len() - upper_level_sst_ids.len();
             debug_assert_eq!(&new_state.l0_sstables[new_l0_len..], upper_level_sst_ids);
@@ -132,8 +245,29 @@ impl LeveledCompactionController {
         }
 
         debug_assert_eq!(new_state.levels[lower_level - 1].0, lower_level);
-        debug_assert_eq!(&new_state.levels[lower_level - 1].1, lower_level_sst_ids);
-        output.clone_into(&mut new_state.levels[lower_level - 1].1);
+
+        let lower_level = &mut new_state.levels[lower_level - 1].1;
+
+        eprintln!("old_lower_level: {lower_level:?}");
+        eprintln!("lower_level_sst_ids: {lower_level_sst_ids:?}");
+        let start = {
+            let (lower_key, _) = self.lower_upper_key(snapshot, upper_level_sst_ids);
+            let mut left = lower_level
+                .partition_point(|id| snapshot.sstables[id].first_key().raw_ref() <= lower_key)
+                .saturating_sub(1);
+            if let Some(id) = lower_level.get(left) {
+                if snapshot.sstables[id].last_key().raw_ref() < lower_key {
+                    left += 1;
+                }
+            }
+            left
+        };
+        eprintln!("new_lower_level: {lower_level:?}");
+        let end = start + lower_level_sst_ids.len();
+        let old = lower_level.splice(start..end, output.to_vec());
+        let old = old.collect::<Vec<_>>();
+        debug_assert_eq!(&old, lower_level_sst_ids);
+        eprintln!("new_lower_level: {lower_level:?}");
 
         (new_state, deleted_sst_ids)
     }
