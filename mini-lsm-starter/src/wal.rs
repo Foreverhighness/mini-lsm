a7d7a442e7935f3f3d7231b370dc6a37d73da884
Foreverhighness <Foreverhighness@github.com>
2024-06-28 09:27:31 +0800

    ✨ (wal): Add wal implement.

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -1,14 +1,17 @@
 #![allow(dead_code)] // REMOVE THIS LINE after fully implementing this functionality
 
-use std::fs::File;
-use std::io::BufWriter;
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
+
+const SIZE_KEY_LEN: usize = std::mem::size_of::<u16>();
+const SIZE_VALUE_LEN: usize = std::mem::size_of::<u16>();
 
 #[derive(Debug)]
 pub struct Wal {
@@ -16,19 +19,63 @@ pub struct Wal {
 }
 
 impl Wal {
-    pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        let file = OpenOptions::new()
+            .append(true)
+            .create_new(true)
+            .open(path)?;
+        let file = Arc::new(Mutex::new(BufWriter::new(file)));
+
+        Ok(Wal { file })
     }
 
-    pub fn recover(_path: impl AsRef<Path>, _skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
-        unimplemented!()
+    pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
+        let path = path.as_ref();
+        let mut file = OpenOptions::new().read(true).open(path)?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+
+        let mut buf = &buf[..];
+        while !buf.is_empty() {
+            let key_len = buf.get_u16() as usize;
+            let key = Bytes::copy_from_slice(&buf[..key_len]);
+            buf.advance(key_len);
+
+            let value_len = buf.get_u16() as usize;
+            let value = Bytes::copy_from_slice(&buf[..value_len]);
+            buf.advance(value_len);
+
+            debug_assert!(!key.is_empty());
+            skiplist.insert(key, value);
+        }
+
+        let file = OpenOptions::new().append(true).open(path)?;
+        let file = Arc::new(Mutex::new(BufWriter::new(file)));
+        Ok(Wal { file })
     }
 
-    pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        let mut buf = Vec::with_capacity(SIZE_KEY_LEN + key.len() + SIZE_VALUE_LEN + value.len());
+
+        let key_len = key.len().try_into().unwrap();
+        let value_len = value.len().try_into().unwrap();
+
+        buf.put_u16(key_len);
+        buf.put_slice(key);
+        buf.put_u16(value_len);
+        buf.put_slice(value);
+
+        let mut file = self.file.lock();
+        file.write_all(&buf)?;
+
+        Ok(())
     }
 
     pub fn sync(&self) -> Result<()> {
-        unimplemented!()
+        let mut file = self.file.lock();
+        file.flush()?;
+        file.get_ref().sync_all()?;
+
+        Ok(())
     }
 }
