635ff069fd7e0b30464f47afd46cfb56bd678aba
Foreverhighness <Foreverhighness@github.com>
2024-06-28 08:29:11 +0800

    🐛 (manifest): Use Deserializer to decode multiple records.

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -17,7 +17,7 @@ pub struct Manifest {
     file: Arc<Mutex<File>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum ManifestRecord {
     Flush(usize),
     NewMemtable(usize),
@@ -38,8 +38,10 @@ impl Manifest {
     pub fn recover(path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
         let path = path.as_ref();
         let file = std::fs::OpenOptions::new().read(true).open(path)?;
-        let buffer_file = BufReader::new(file);
-        let records = serde_json::from_reader(buffer_file)?;
+        let reader = BufReader::new(file);
+        let records = serde_json::Deserializer::from_reader(reader)
+            .into_iter()
+            .collect::<serde_json::Result<_>>()?;
 
         let file = std::fs::OpenOptions::new().append(true).open(path)?;
         let file = Arc::new(Mutex::new(file));
@@ -63,5 +65,35 @@ impl Manifest {
         file.sync_all()?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_record_serde() {
+        let record = ManifestRecord::Flush(1);
+        let json = serde_json::to_vec(&record).unwrap();
+        let back = serde_json::from_slice(&json).unwrap();
+        assert_eq!(record, back);
+    }
+
+    #[test]
+    fn test_two_record() {
+        let record1 = ManifestRecord::Flush(1);
+        let record2 = ManifestRecord::Flush(2);
+        let mut json1 = serde_json::to_vec(&record1).unwrap();
+        let json2 = serde_json::to_vec(&record2).unwrap();
+        json1.extend(json2);
+
+        let mut two_record = Vec::with_capacity(2);
+        let de = serde_json::Deserializer::from_slice(&json1);
+        for record in de.into_iter() {
+            let record: ManifestRecord = record.unwrap();
+            two_record.push(record);
+        }
+        assert_eq!(two_record, vec![record1, record2]);
     }
 }
