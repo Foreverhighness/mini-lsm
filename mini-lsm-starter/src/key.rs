f92b7de75350d9146b70645b38f44e1ba089c0b4
Foreverhighness <Foreverhighness@github.com>
2024-06-29 21:55:10 +0800

    ✨ (key): Add helper function.

--- mini-lsm-starter/src/key.rs
+++ mini-lsm-starter/src/key.rs
@@ -1,12 +1,5 @@
 mod raw_key;
 mod timestamped_key;
 
-// use raw_key as key;
-use timestamped_key as key;
-
-pub const TS_ENABLED: bool = key::TS_ENABLED;
-
-pub type Key<T> = key::Key<T>;
-pub type KeySlice<'a> = key::KeySlice<'a>;
-pub type KeyVec = key::KeyVec;
-pub type KeyBytes = key::KeyBytes;
+// pub use raw_key::*;
+pub use timestamped_key::*;
