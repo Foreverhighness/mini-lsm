0b0f92e2512e4f5e719a868b95674314fc69589e
Foreverhighness <Foreverhighness@github.com>
2024-07-01 00:17:31 +0800

    ✨ (timestamp): fallback to default value.

--- mini-lsm-starter/src/key/timestamp.rs
+++ mini-lsm-starter/src/key/timestamp.rs
@@ -1,9 +1,11 @@
+use super::TS_ENABLED;
+
 pub type TimeStamp = u64;
 
 /// Temporary, should remove after implementing full week 3 day 1 + 2.
 pub const TS_DEFAULT: TimeStamp = 0;
 
-pub const TS_MAX: TimeStamp = u64::MAX;
-pub const TS_MIN: TimeStamp = u64::MIN;
-pub const TS_RANGE_BEGIN: TimeStamp = u64::MAX;
-pub const TS_RANGE_END: TimeStamp = u64::MIN;
+pub const TS_MAX: TimeStamp = if TS_ENABLED { u64::MAX } else { TS_DEFAULT };
+pub const TS_MIN: TimeStamp = if TS_ENABLED { u64::MIN } else { TS_DEFAULT };
+pub const TS_RANGE_BEGIN: TimeStamp = if TS_ENABLED { u64::MAX } else { TS_DEFAULT };
+pub const TS_RANGE_END: TimeStamp = if TS_ENABLED { u64::MIN } else { TS_DEFAULT };
