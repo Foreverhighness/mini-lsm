b6f7e3c92e27663a054a85584aaf254bebd54f79
Foreverhighness <Foreverhighness@github.com>
2024-07-02 19:20:47 +0800

    ✨ (watermark): Add watermark.

--- mini-lsm-starter/src/mvcc/watermark.rs
+++ mini-lsm-starter/src/mvcc/watermark.rs
@@ -1,12 +1,6 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-#![allow(clippy::unused_self)] // TODO(fh): remove clippy allow
-#![allow(clippy::unnecessary_wraps)] // TODO(fh): remove clippy allow
-#![allow(clippy::missing_const_for_fn)] // TODO(fh): remove clippy allow
-
 use std::collections::BTreeMap;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Watermark {
     readers: BTreeMap<u64, usize>,
 }
@@ -18,11 +12,25 @@ impl Watermark {
         }
     }
 
-    pub fn add_reader(&mut self, ts: u64) {}
+    pub fn add_reader(&mut self, ts: u64) {
+        *self.readers.entry(ts).or_default() += 1;
+    }
 
-    pub fn remove_reader(&mut self, ts: u64) {}
+    pub fn remove_reader(&mut self, ts: u64) {
+        debug_assert!(self.readers.contains_key(&ts));
+
+        let cnt = self.readers.get_mut(&ts).unwrap();
+        *cnt -= 1;
+        if *cnt == 0 {
+            self.readers.remove(&ts);
+        }
+    }
 
     pub fn watermark(&self) -> Option<u64> {
-        Some(0)
+        self.readers.first_key_value().map(|(&ts, _)| ts)
+    }
+
+    pub fn num_retained_snapshots(&self) -> usize {
+        self.readers.len()
     }
 }
